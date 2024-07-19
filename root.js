8a8da5328e3fa177cfcddcfc14dbb0f38faeea01
batyrrrr <bolatbekbatyr41@gmail.com>
2024-07-19 11:00:39 +0500

    snd commit

--- root.js
+++ root.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { handler } = require('./controller/index');
 const serverless = require('serverless-http')
 
-// const PORT = process.env.PORT || 4040;
+const PORT = process.env.PORT || 4040;
 
 const app = express();
 app.use(express.json())
@@ -16,10 +16,10 @@ app.get('*', async (req, res) => {
   res.send(await handler(req, "GET"));
 })
 
-// app.listen(PORT, function (err) {
-//   if (err) console.log(err)
-//   console.log("Server listening on PORT", PORT);
-// })
+app.listen(PORT, function (err) {
+  if (err) console.log(err)
+  console.log("Server listening on PORT", PORT);
+})
 
 
-module.exports.handler = serverless(app)
+module.exports = app
